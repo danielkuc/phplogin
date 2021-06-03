bf111ae776bed8ae2dc640651458466c3949c7db
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-06-03 22:56:33 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Php Project</title>
+  <!-- link to font -->
+  <!-- link to reset -->
+  <!-- link to stylesheet -->
+</head>
+<body>
+  
+  <nav>
+    <div class="wrapper">
+      <a href="index.php" alt="logo">Add logo</a>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="discover.php">About Us</a></li>
+        <li><a href="blog.php.php">Find Blog</a></li>
+        <li><a href="signup.php">Sign Up</a></li>
+        <li><a href="login.php">Log in</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="wrapper">
+  
+  <section class="index-intro">
+    <h1>This is an Introduction</h1>
+    <p>Here is an important paragraph that explains the purpose of the website</p>
+  </section>
+
+  <section class="index-categories">
+    <h2>Some basic categories</h2>
+    <div class="index-categories-list">
+      <div>
+        <h3>category 1</h3>
+      </div>
+      <div>
+        <h3>category 2</h3>
+      </div>
+      <div>
+        <h3>category 3</h3>
+      </div>
+      <div>
+        <h3>category 4</h3>
+      </div>
+    </div>
+  </section>
+  
+  </div>
+
+
+</body>
+</html>
