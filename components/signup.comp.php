e724cf8baae1bc15a453c9a09d530b30c872063a
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-06-05 08:07:37 +0100

    minor changes

--- components/signup.comp.php
+++ components/signup.comp.php
@@ -7,7 +7,15 @@ if(isset($_POST["submit"])) {
   $pwd = $_POST["pwd"];
   $pwdRepeat = $_POST["pwdrepeat"];
   
+  require_once 'dbh.comp.php';
+  require_once 'functions.comp.php';
+
+  if(emptyInput() !== false) {
+    header("location: ../signup.php?error=emptyinput");
+    exit();    
+  }
 
 } else {
   header("location: ../signup.php");
+  exit();
 }
--- signup.php
+++ signup.php
@@ -8,7 +8,7 @@
   <input type="text" name="name" placeholder="Full Name">  
   <input type="email" name="email" placeholder="E-mail">  
   <input type="text" name="uid" placeholder="Username">  
-  <input type="password" name="pwd" placeholder="Password">  
+  <input type="password" name="pwd" placeholder="Password">
   <input type="password" name="pwdrepeat" placeholder="Confirm Password">
   <button type="submit" name="submit">Sign Up</button>  
   </form>
