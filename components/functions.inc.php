8aeedd40de0e23eb579bd50e35d7d1ecf123df4d
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-06-06 21:16:24 +0100

    added first checks

--- components/functions.inc.php
+++ components/functions.inc.php
@@ -1,1 +1,25 @@
 <?php
+
+function emptyInput($name, $email, $username, $pwd, $pwdRepeat) {
+  $result;
+
+  if(empty($name) || empty($email) || empty($username) || empty($username) || empty($pwd) || empty($pwdRepeat)) {
+    $result = true;
+  } else {
+    $result = false;
+  }
+
+  return $result;
+};
+
+function invalidUid($username) {
+  $result;
+
+  if(!preg_match("/^[a-zA-Z0-9]*$/"), $username) {
+    $result = true;
+  } else {
+    $result = false;
+  }
+
+  return $result;
+};
--- components/signup.comp.php
+++ components/signup.comp.php
@@ -10,9 +10,30 @@ if(isset($_POST["submit"])) {
   require_once 'dbh.comp.php';
   require_once 'functions.comp.php';
 
-  if(emptyInput() !== false) {
+  if(emptyInput($name, $email, $username, $pwd, $pwdRepeat) !== false) {
     header("location: ../signup.php?error=emptyinput");
-    exit();    
+    exit();
+    
+    if(invalidUid($username) !== false) {
+      header("location: ../signup.php?error=invaliduid");
+      exit();
+    };
+    if(invalidEmail($email) !== false) {
+      header("location: ../signup.php?error=invaliduid");
+      exit();
+    };
+    if(pwdmatch($pwd, $pwdRepeat)) {
+      header("location: ../signup.php?error=pwdinvalid");
+      exit();
+    };
+    if(uidExists($conn, $username)) {
+      header("location: ../signup.php?error=uidtaken");
+      exit();
+    };
+
+    createUser($conn, $name, $email, $username, $pwd) {
+
+    }
   }
 
 } else {
