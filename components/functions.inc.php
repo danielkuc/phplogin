e62c5ab9d2dcb088ffc5a7e76de64977c9ed8e71
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-06-07 11:30:36 +0100

    added more functions

--- components/functions.inc.php
+++ components/functions.inc.php
@@ -23,3 +23,14 @@ function invalidUid($username) {
 
   return $result;
 };
+
+
+function invalidEmail($email) {
+  $result;
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $result = true;
+  } else {
+    $result = false;
+  }
+  return $result;
+};
